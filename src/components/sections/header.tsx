aaed5f1c8ee5e9a4f52ba4fb821462f27672f1e5
Trejo Torres Fernando <fernando.trejo.enp5.159@gmail.com>
2025-08-17 14:51:14 +0000

    ajusta el header en mobile para que se distribuya

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -91,22 +91,18 @@ const ContactDialog = () => (
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center mx-auto">
-        <div className="mr-4 hidden md:flex">
-          <Link href="/" className="mr-6 flex items-center space-x-2">
-            <DfacLogo />
-          </Link>
-        </div>
-        <div className="md:hidden mr-4">
-          <Link href="/" className="flex items-center space-x-2">
-             <DfacLogo />
-          </Link>
-        </div>
-        <nav className="hidden md:flex items-center space-x-6 text-sm font-medium ml-auto">
-          <NavLinks />
-        </nav>
-        <div className="flex items-center justify-end md:ml-6">
+      <div className="container flex h-16 items-center justify-between mx-auto">
+        <Link href="/" className="flex items-center space-x-2">
+          <DfacLogo />
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
+            <NavLinks />
+          </nav>
+
           <ContactDialog />
+
           <div className="md:hidden">
             <Sheet>
               <SheetTrigger asChild>
