ed83e19f7863e02e798d72847b17da0b59be98ce
Trejo Torres Fernando <fernando.trejo.enp5.159@gmail.com>
2025-08-24 22:14:32 +0000

    cambios

--- src/components/icons/dfac-logo.tsx
+++ src/components/icons/dfac-logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export function DfacLogo() {
   return (
     <Image
-      src="https://cimbrayaccesorios.com.mx/wp-content/uploads/2020/09/Recurso-2.png.webp"
+      src="/image/Recurso-2.png.webp"
       alt="DFAC Logo"
       width={130}
       height={45}
